79719c1ce0b44448299b21c8430a75893769e330
Your Name <jack.spicer@colorado.edu>
2019-02-24 02:25:26 -0700

    added room.py

--- engine/room.py
+++ engine/room.py
@@ -1,13 +1,42 @@
-class Room():
-    def __init__(self, things):
-        self.things = things
-        self.functions = []
+# class Room():
+# 	def __init__(self, things):
+# 		self.things = things
+# 		self.functions = []
 
-    def getActions(self):
-        for t in self.things:
-            self.functions[v] = self.functions.get(v, [])
-            self.functions[v].append(k)
+# 	def getActions(self):
+# 		for t in self.things:
+# 			self.functions[v] = self.functions.get(v, [])
+# 			self.functions[v].append(k)
+
+class Outside(Room):
+	def __init__(self):
+		self.room_actions = {
+
+
+		}
+
+	def updateState(self, game_state):
+		if()
+
+	def evaluate(self, game_state, room, inventory):
+
+		
+class Foyer(Room):
+	def __init__(self, things):
+		super().__init__(things)
 
 class DiningRoom(Room):
-    def __init__(self, things):
-        super().__init__(things)
+	def __init__(self, things):
+		super().__init__(things)
+
+class Kitchen(Room):
+	def __init__(self, things):
+		super().__init__(things)
+
+class CoatRoom(Room):
+	def __init__(self, things):
+		super().__init__(things)
+
+class Hallway(Room):
+	def __init__(self, things):
+		super().__init__(things)
